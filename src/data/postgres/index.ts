e2290252c7124b257545532a5d68cfe75c4015c1
angelmartinezcastrojs-dev <angel.martinez.castrojs@gmail.com>
2026-02-09 00:10:27 -0600

    fix prismaClient

--- src/data/postgres/index.ts
+++ src/data/postgres/index.ts
@@ -2,12 +2,12 @@ import { PrismaClient } from '../../generated/client';
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg';
 
-
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL
+  // CAMBIO CLAVE: Asegúrate de que Railway tenga esta variable
+  connectionString: process.env.DATABASE_URL 
 })
 
 const adapter = new PrismaPg(pool)
 
-
+// Aquí TypeScript no se quejará porque le estás pasando el adapter
 export const prisma = new PrismaClient({ adapter });
